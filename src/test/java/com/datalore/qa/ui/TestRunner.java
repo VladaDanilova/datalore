0a2035649a637a1c27ea29ea19323056870ee548
vladadanilova <just.you177@gmail.com>
2023-02-20 20:21:48 +0300

    fixes

---
+++ src/test/java/com/datalore/qa/ui/TestRunner.java
@@ -0,0 +1,8 @@
+package com.datalore.qa.ui;
+
+import com.github.valfirst.jbehave.junit.monitoring.JUnitReportingRunner;
+import org.junit.runner.RunWith;
+
+@RunWith(JUnitReportingRunner.class)
+public class TestRunner {
+}
